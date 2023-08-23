60447e608694d176c88700f9b1fbc953d75040dd
BaykovAleksandr <baykov-1988@mail.ru>
2023-08-23 23:08:20 +0300

    add requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
 
     yield
-
     browser.quit()
