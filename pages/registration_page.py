c583f7b3d17c3e71afe83600d59918de43975d43
BaykovAleksandr <baykov-1988@mail.ru>
2023-08-23 19:40:21 +0300

    Initial commit

---
+++ pages/registration_page.py
@@ -0,0 +1,60 @@
+import os
+
+from selene import browser, be, have
+
+
+class RegistrationPage:
+
+    def open(self):
+        browser.open('/automation-practice-form')
+
+    def fill_first_name(self, name):
+        browser.element('#firstName').should(be.blank).type(name)
+
+    def fill_second_name(self, family):
+        browser.element('#lastName').should(be.blank).type(family)
+
+    def fill_email(self, email):
+        browser.element('#userEmail').should(be.blank).type(email)
+
+    def choose_gender(self):
+        browser.element('[for="gender-radio-1"]').click()
+
+    def fill_phone_number(self, phone):
+        browser.element('#userNumber').should(be.blank).type(phone)
+
+    def choose_birthday(self):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(".react-datepicker__month-select>option[value='11']").click()
+        browser.element(".react-datepicker__year-select>option[value='1988']").click()
+        browser.element('.react-datepicker__day--019').click()
+
+    def fill_interests(self, subject):
+        browser.element('#subjectsInput').should(be.blank).type(subject).press_enter()
+
+    def choose_hobbies(self):
+        browser.element("[for='hobbies-checkbox-3']").click()
+
+    def upload_file(self, path):
+        browser.element('#uploadPicture').send_keys(
+            os.path.abspath(path))
+
+    def fill_current_address(self, address):
+        browser.element('#currentAddress').type(address)
+
+    def choose_state(self, state):
+        browser.element('#react-select-3-input').type(state).press_enter()
+
+    def choose_city(self, city):
+        browser.element('#react-select-4-input').type(city).press_enter()
+
+    def click_submit(self):
+        browser.element('#submit').press_enter()
+
+    def assert_title(self, text):
+        browser.element('#example-modal-sizes-title-lg').should(have.text(text))
+
+    def assert_body(self, result):
+        browser.element('.table').should(have.text(result))
+
+
---
+++ tests/test_automation_practice_form.py
@@ -0,0 +1,44 @@
+
+from pages.registration_page import RegistrationPage
+
+
+def test_positive(open_browser):
+    registration_page = RegistrationPage()
+
+    registration_page.open()
+
+    registration_page.fill_first_name('Aleksandr')
+
+    registration_page.fill_second_name('Baykov')
+
+    registration_page.fill_email('test@yandex.com')
+
+    registration_page.choose_gender()
+
+    registration_page.fill_phone_number('1234567890')
+
+    registration_page.choose_birthday()
+
+    registration_page.fill_interests('Maths')
+
+    registration_page.choose_hobbies()
+
+    registration_page.upload_file('files/san-diego-night-view-united-states-boats.jpeg')
+
+    registration_page.fill_current_address("Moscow Region")
+
+    registration_page.choose_state('ncr')
+
+    registration_page.choose_city('Delhi')
+
+    registration_page.click_submit()
+
+    registration_page.assert_title('Thanks for submitting the form')
+
+    registration_page.assert_body('Aleksandr Baykov' and 'test@yandex.com' and 'Male' and
+                                                   '1234567890' and '19 December,1988' and 'Maths' and 'Music' and
+                                                   'san-diego-night-view-united-states-boats.jpeg' and
+                                                   'Moscow Region' and 'NCR Delhi')
+
+
+
